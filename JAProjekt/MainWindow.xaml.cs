982f145b65d15bf1e9509b0ffdba91fee8f1c80b
Yaster126 <grigo112@wp.pl>
2023-01-25 04:30:19 +0100

    Odzyskiwanie

--- JAProjekt/MainWindow.xaml.cs
+++ JAProjekt/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Media;
 
 namespace JAProjekt
 {
@@ -21,17 +23,66 @@ namespace JAProjekt
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private SoundPlayer player;
+		private string inputPath = "";
+		private string outputPath = "";
+
+
+		public void WriteWav(float[] floatOutput)
+		{
+		
+		}
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			Thread.Value = Environment.ProcessorCount;
 		}
 
 		private void Choose_Click(object sender, RoutedEventArgs e)
 		{
 			OpenFileDialog openFileDialog = new OpenFileDialog();
 			openFileDialog.Filter = "Audio file (*.wav)|*wav";
-			if (openFileDialog.ShowDialog() == true) ;
+			if (openFileDialog.ShowDialog() == true)
+			{
+				inputPath = openFileDialog.FileName;
+				Title.Content = openFileDialog.SafeFileName;
 
+				Play_Stereo.IsEnabled = true;
+			}
+
+
+		}
+
+		private void Play_Stereo_Click(object sender, RoutedEventArgs e)
+		{
+			player = new(inputPath);
+			player.Play();
+
+			Stop.IsEnabled = true;
+		}
+
+		private void Play_Mono_Click(object sender, RoutedEventArgs e)
+		{
+			//TODO
+		}
+
+		private void Stop_Click(object sender, RoutedEventArgs e)
+		{
+			player.Stop();
+			Stop.IsEnabled = false;
+		}
+
+		private void Run_Click(object sender, RoutedEventArgs e)
+		{
+			if(CSharp.IsChecked == true)
+			{
+
+			}
+			else if(ASM.IsChecked == true)
+			{
+
+			}
 		}
 	}
 }
