efdffe3d0ca009d0b0d64a3bb196c3d18ca58601
Yaster126 <grigo112@wp.pl>
2023-01-25 06:54:17 +0100

    kolejny

--- JAProjekt/MainWindow.xaml.cs
+++ JAProjekt/MainWindow.xaml.cs
@@ -1,22 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Media;
-using NAudio;
 using NAudio.Wave;
+using System.Runtime.InteropServices;
+using StereoToMonoConverterCSharp;
 
 namespace JAProjekt
 {
@@ -25,37 +13,54 @@ namespace JAProjekt
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		private SoundPlayer player;
+		//private SoundPlayer player;
 		private string inputPath = "";
 		private string outputPath = "";
 		private int sampleRate = 0;
 		private int bitsPerSample = 0;
+		private int channels = 0;
 		private float[] Stereo = Array.Empty<float>();
 		private float[] Mono = Array.Empty<float>();
+
+		[DllImport(@"C:\Grzesia\Projekty\Visual Studio 2022\JAProjekt\x64\Debug\StereoToMonoConverterASM.dll")]
+		static extern int StereoToMonoAsm(int a, int b);
 
 
 		public void ReadWav(string path)
 		{
-			using (MediaFoundationReader media = new MediaFoundationReader(path))
+			using MediaFoundationReader media = new MediaFoundationReader(path);
+			
+			sampleRate = media.WaveFormat.SampleRate;
+			bitsPerSample = media.WaveFormat.BitsPerSample;
+			channels = media.WaveFormat.Channels;
+
+			int byteLength = (int)media.Length * 2;
+			int tabSize = byteLength / sizeof(float);
+
+			IWaveProvider stream = new Wave16ToFloatProvider(media);
+			WaveBuffer _waveBuffer = new(byteLength);
+			stream.Read(_waveBuffer, 0, byteLength);
+			Stereo = new float[tabSize];
+
+			for (int i = 0; i < tabSize; i++)
 			{
-				int _byteBuffer32_length = (int)media.Length * 2;
-				int _floatBuffer_length = _byteBuffer32_length / sizeof(float);
-
-				IWaveProvider stream32 = new Wave16ToFloatProvider(media);
-				WaveBuffer _waveBuffer = new(_byteBuffer32_length);
-				stream32.Read(_waveBuffer, 0, (int)_byteBuffer32_length);
-				Stereo = new float[_floatBuffer_length];
-
-				for (int i = 0; i < _floatBuffer_length; i++)
-				{
-					Stereo[i] = _waveBuffer.FloatBuffer[i];
-				}
+				Stereo[i] = _waveBuffer.FloatBuffer[i];
 			}
 		}
 
-		public void WriteWav(float[] floatOutput)
+		public void GenerateFileName()
 		{
-			WaveFormat waveFormat = new WaveFormat(sampleRate, bitsPerSample, 1);
+			int idx = inputPath.LastIndexOf('.');
+
+			if (idx != -1)
+			{
+				outputPath = string.Concat(inputPath.AsSpan(0, idx), "_MONO", inputPath.AsSpan(idx + 1));
+			}
+		}
+
+	public void WriteWav(float[] floatOutput)
+		{
+			WaveFormat waveFormat = new(sampleRate, bitsPerSample, 1);
 			using WaveFileWriter writer = new(outputPath, waveFormat);
 			writer.WriteSamples(floatOutput, 0, floatOutput.Length);
 		}
@@ -83,7 +88,7 @@ namespace JAProjekt
 
 		private void Play_Stereo_Click(object sender, RoutedEventArgs e)
 		{
-			player = new(inputPath);
+			SoundPlayer player = new(inputPath);
 			player.Play();
 
 			Stop.IsEnabled = true;
@@ -96,6 +101,7 @@ namespace JAProjekt
 
 		private void Stop_Click(object sender, RoutedEventArgs e)
 		{
+			SoundPlayer player = new();
 			player.Stop();
 			Stop.IsEnabled = false;
 		}
@@ -104,7 +110,7 @@ namespace JAProjekt
 		{
 			if(CSharp.IsChecked == true)
 			{
-
+				Mono = StereoToMonoConverter(Stereo);
 			}
 			else if(ASM.IsChecked == true)
 			{
